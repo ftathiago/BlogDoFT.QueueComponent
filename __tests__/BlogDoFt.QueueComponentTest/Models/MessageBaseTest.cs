359ed93218b7e5866b2f74aced3d5b43af2b65c6
Francisco Thiago de Almeida <ftathiago@gmail.com>
2020-11-01 19:29:09 -0300

    feat: Add a MessageBase class. Closes #5 (#6)

---
+++ __tests__/BlogDoFt.QueueComponentTest/Models/MessageBaseTest.cs
@@ -0,0 +1,79 @@
+﻿using BlogDoFt.QueueComponentTest.Fixtures;
+using Bogus;
+using FluentAssertions;
+using System;
+using System.Text.Json;
+using Xunit;
+
+namespace BlogDoFt.QueueComponentTest.Models
+{
+    public class MessageBaseTest
+    {
+        private readonly Faker _faker;
+
+        public MessageBaseTest() =>
+            _faker = FakerFixture.GetFaker();
+
+        [Fact]
+        public void ShouldBeValidWhenMessageIsValid()
+        {
+            // Given
+            var message = GetValidMessage();
+
+            // When
+            var valid = message.IsValid();
+
+            // Then
+            valid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldBeInvalidWhenMessageIsInvalid()
+        {
+            // Given
+            var message = GetValidMessage();
+            message.RequiredField = string.Empty;
+
+            // When
+            var valid = message.IsValid();
+
+            // Then
+            valid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldDefinePayload()
+        {
+            // Given
+            var message = GetValidMessage();
+            var payload = JsonSerializer.Serialize(message);
+
+            // When
+            message.SetPayload(payload);
+
+            // Then
+            message.Payload.Should().Be(payload);
+        }
+
+        [Fact]
+        public void ShouldDefineCreationTimestamp()
+        {
+            // Given
+            var message = GetValidMessage();
+            var dateTime = DateTimeOffset.Now;
+            var expectedTimestamp = dateTime.ToFileTime();
+
+            // When
+            message.SetCreationTimeStamp(dateTime);
+
+            // Then
+            message.CreationTimeStamp.Should().Be(expectedTimestamp);
+        }
+
+        private MessageStub GetValidMessage() => new MessageStub
+        {
+            NonRequiredField = _faker.Random.Int(1),
+            RequiredField = _faker.Lorem.Sentence(),
+        };
+    }
+}
---
+++ src/BlogDoFt.QueueComponent/Models/MessageBase.cs
@@ -0,0 +1,20 @@
+﻿using BlogDoFt.QueueComponent.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BlogDoFt.QueueComponent
+{
+    public abstract class MessageBase : ValidatableModel
+    {
+        public Dictionary<string, string> Headers { get; } = new Dictionary<string, string>();
+
+        public long CreationTimeStamp { get; private set; } = DateTimeOffset.Now.ToFileTime();
+
+        public string Payload { get; private set; }
+
+        internal void SetPayload(string payload) => Payload = payload;
+
+        internal void SetCreationTimeStamp(DateTimeOffset creationTimeStamp) =>
+            CreationTimeStamp = creationTimeStamp.ToFileTime();
+    }
+}
